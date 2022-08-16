7be967c99cab67f6bb22bddddb847cfa41048aa7
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-08-16 14:19:30 -0300

    Update v1.products.js

--- src/services/v1.products.js
+++ src/services/v1.products.js
@@ -4,9 +4,11 @@ const XLSX = require("XLSX");
 module.exports = function(app, routerPublic, routerPrivate)
 {
 	var upload = app.get("upload");
-	routerPublic.post("/", upload.single("sheet"), (req, res) => {
+	var db = app.get("db");	
+	routerPublic.post(["/upload/xlsx", "/upload/csv"], upload.single("sheet"), (req, res) => {
 		const wb = XLSX.read(req.file.buffer);
 		const data = wb.SheetNames.map(SheetName => XLSX.utils.sheet_to_json(wb.Sheets[SheetName]));
+		data.forEach(sheet => db.loadProducts(sheet));
 		res.send(200);
 	});
 
