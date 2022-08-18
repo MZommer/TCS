35bb099583bfd1c17482405c2c22a37b9cce0a09
MZommer <mazokra@gmail.com>
2022-08-18 00:10:04 -0300

    Updated DB lib with ORM

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,79 @@
-const { promisify } = require('util');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./sql.db');
-console.log("Connected to SQLite database");
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './sql.db',
+    logging: console.log,
+  });
 
-const db_all = promisify(db.all).bind(db);
-const db_get = promisify(db.get).bind(db);
-const db_exec = promisify(db.exec).bind(db);
-const db_run = promisify(db.run).bind(db);
+sequelize.authenticate()
+    .then(() => console.log('Connection has been established successfully.'))
+    .catch(error => console.error('Unable to connect to the database:', error))
+// Connection
+
+const ProductModel = sequelize.define("Product", {
+    ID: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+    },
+    Name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    Brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    Description: {
+        type: DataTypes.STRING,
+    },
+    Price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    Thumb: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    Cover: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    EnvID: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+},
+{
+    timestamps: true,
+})
+const Ingridient = {
+    type: DataTypes.STRING,
+    defaultValue: "0g",
+}
+const Ingridients = ["TotalFat", "SaturatedFat", "TransFat", "Cholesterol", "Sodium", "Sugar", "AddedSugar", "Protein", "Fiber", "Calcium", "Iron", "VitaminA", "VitaminB", "VitaminD", "Potassium", "ServingSize"]
+
+const NutritionalTableModel = sequelize.define("NutritionalTable", {
+    ProductID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: ProductModel,
+            key: "ID",
+        },
+    },
+    Calories: {
+        type: DataTypes.STRING,
+        defaultValue: "0kcal",
+    },
+    ...Ingridients.reduce((obj, ing) => obj[ing] = Ingridient, {})
+})
+// Loading models
+
+await sequelize.sync({ force: true });
+console.log("All models were synchronized successfully.");
+// Syncronizing models
 
 module.exports = class DB {
     
