06bf83fd767756788c89bf40796aaf5e639983cc
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-11-16 12:07:19 +0000

    Fixes

--- services/v1.config.js
+++ services/v1.config.js
@@ -1,19 +1,18 @@
 "use strict";
 
-module.exports = function(app, routerPublic, routerPrivate)
-{
-	var db = app.get("db");	
-	var Tempest = app.get("Tempest");
-	routerPublic.get("/", async (req, res, next) => {
-		const config = await db.getConfig(req.query.SpaceID).catch(err => res.send(401))
-		res.json(config);
-	})
+module.exports = function(app, routerPublic, routerPrivate) {
+    var db = app.get("db");
+    var Tempest = app.get("Tempest");
+    routerPublic.get("/", async (req, res, next) => {
+        const config = await db.getConfig(req.query.SpaceID).catch(console.error)
+        res.json(config);
+    })
 
-	routerPublic.post("/createEnv", async (req, res, next) => {
-		await db.createEnv(req.query.EnvID).catch(err => res.send(401))
-	})
-	routerPublic.post("/createSpace", async (req, res, next) => {
-		await db.createSpace(req.body).catch(err => res.send(401))
-	})
-	
+    routerPublic.post("/createEnv", async (req, res, next) => {
+        await db.createEnv(req.query.EnvID).catch(err => res.send(401))
+    })
+    routerPublic.post("/createSpace", async (req, res, next) => {
+        await db.createSpace(req.body).catch(err => res.send(401))
+    })
+
 }
